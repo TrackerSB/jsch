0c08ad8730485b383dee2bf3965f43f629bba5dc
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-08-06 15:28:03 +0200

    Modularize JSch

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module bayern.steinbrecher.jsch {
+    exports bayern.steinbrecher.jsch;
+    exports bayern.steinbrecher.jsch.jce;
+    exports bayern.steinbrecher.jsch.jcraft;
+    exports bayern.steinbrecher.jsch.jgss;
+
+    requires jzlib;
+}
