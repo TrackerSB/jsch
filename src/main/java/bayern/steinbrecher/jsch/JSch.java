a4c9575ae794658e4d20b67ae97eaabaa577becc
Stefan Huber <stefan.huber.niedling@outlook.com>
2020-09-22 15:54:33 +0200

    Prepare for release of version 0.1.57

--- src/main/java/bayern/steinbrecher/jsch/JSch.java
+++ src/main/java/bayern/steinbrecher/jsch/JSch.java
@@ -36,7 +36,7 @@ public class JSch {
     /**
      * The version number.
      */
-    public static final String VERSION = "0.1.54";
+    public static final String VERSION = "0.1.57";
 
     static java.util.Hashtable config = new java.util.Hashtable();
 
